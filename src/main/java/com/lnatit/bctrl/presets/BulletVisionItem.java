d730a7ae3255b5babcb55ba9e158ab7162b0d73d
Locus_Natit <3113135718@qq.com>
2023-07-23 00:42:40 +0800

    Add item tags

--- src/main/java/com/lnatit/bctrl/presets/BulletVisionItem.java
+++ src/main/java/com/lnatit/bctrl/presets/BulletVisionItem.java
@@ -1,6 +1,8 @@
 package com.lnatit.bctrl.presets;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
@@ -8,10 +10,11 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
-public class BulletVisionItem extends Item implements Equipable
+public class BulletVisionItem extends ArmorItem implements Equipable
 {
     public static final String ITEM_NAME = "bullet_vision";
     // TODO define item tags in json
@@ -19,7 +22,7 @@ public class BulletVisionItem extends Item implements Equipable
 
     public BulletVisionItem()
     {
-        super(new Properties().stacksTo(1).rarity(Rarity.RARE));
+        super(NotArmor.MATERIAL, Type.HELMET, new Properties().stacksTo(1).rarity(Rarity.RARE));
     }
 
     @Override
@@ -32,5 +35,61 @@ public class BulletVisionItem extends Item implements Equipable
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand)
     {
         return this.swapWithEquipmentSlot(this, level, player, hand);
+    }
+
+    public static class NotArmor implements ArmorMaterial
+    {
+        public static final NotArmor MATERIAL = new NotArmor();
+
+        private NotArmor()
+        {}
+
+        @Override
+        public int getDurabilityForType(@NotNull Type type)
+        {
+            return 0;
+        }
+
+        @Override
+        public int getDefenseForType(@NotNull Type type)
+        {
+            return 0;
+        }
+
+        @Override
+        public int getEnchantmentValue()
+        {
+            return 0;
+        }
+
+        @Override
+        public @NotNull SoundEvent getEquipSound()
+        {
+            return SoundEvents.ARMOR_EQUIP_NETHERITE;
+        }
+
+        @Override
+        public @NotNull Ingredient getRepairIngredient()
+        {
+            return Ingredient.EMPTY;
+        }
+
+        @Override
+        public @NotNull String getName()
+        {
+            return "vision";
+        }
+
+        @Override
+        public float getToughness()
+        {
+            return 0;
+        }
+
+        @Override
+        public float getKnockbackResistance()
+        {
+            return 0;
+        }
     }
 }
